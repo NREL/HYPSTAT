d9b325fa8c10cf844c9bb4cc57864ef3ba248959
Li, Yijin <Yijin.Li@nrel.gov>
2024-09-09 21:19:29 -0600

    change HPC yaml files

--- Case_Study/HPC_run_model_case_study.py
+++ Case_Study/HPC_run_model_case_study.py
@@ -3,7 +3,7 @@ years = [2030, 2040, 2050]
 for year in years:
     print('### Running model for year {y} ###'.format(y=year))
     print()
-    test = HYPSTAT(yaml_file_path='Case_Study_Scenario.yaml',year=year)
+    test = HYPSTAT(yaml_file_path='Case_Study_Scenario_HPC.yaml',year=year)
     test.two_step_solve(solver='gurobi')
     test.write_outputs('Outputs/Case_Study/{year}')
     print('Done!')
