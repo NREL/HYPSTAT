c889117ea56a39232bf8bc782c0055e7da9b7649
yijinli22 <115480649+yijinli22@users.noreply.github.com>
2025-03-27 07:58:32 -0600

    remove the archive folders

--- Archive_250131/Case_Study/HPC_run_model_case_study.py
+++
@@ -1,9 +0,0 @@
-from model_object import HYPSTAT
-years = [2030, 2040, 2050]
-for year in years:
-    print('### Running model for year {y} ###'.format(y=year))
-    print()
-    test = HYPSTAT(yaml_file_path='Case_Study_Scenario_HPC.yaml',year=year)
-    test.two_step_solve(solver='gurobi')
-    test.write_outputs('Outputs/Case_Study/{}'.format(year))
-    print('Done!')
